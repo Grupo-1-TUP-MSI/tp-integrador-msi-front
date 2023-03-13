ae9a0cb207ffb389b861b22891c1c46acaa814b1
Fabrizzio Lo Presti <89172392+FabrizzioLoPresti@users.noreply.github.com>
2023-03-13 17:12:16 -0300

    Update http.api.ts

--- src/api/http.api.ts
+++ src/api/http.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { readToken } from '@app/services/localStorage.service';
 
 export const httpApi = axios.create({
-  baseURL: 'https://pintureria-api.herokuapp.com/',
+  baseURL: 'https://tp-integrador-msi-back-production-f720.up.railway.app/',
 });
 
 httpApi.interceptors.request.use((config) => {
